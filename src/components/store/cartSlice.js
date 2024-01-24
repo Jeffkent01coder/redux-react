213013420eed3f8bafacd62a66f679ee6ae0fb58
jeff <geoffreyerastus956@gmail.com>
2024-01-23 22:52:15 -0500

    cartslice

---
+++ src/components/store/cartSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const cartSlice  = createSlice({
+    name : 'cart',
+    initialState,
+    reducers : {
+        add(state, action){
+            state.push(action.payload)
+        }
+
+    }
+});
+
+export default cartSlice.reducer
